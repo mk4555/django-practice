25096dcf03e65eed0e5988fd8d228d373e9cc427
Min Suk Kim <minkimcodes@gmail.com>
2018-10-17 21:37:49 -0400

    mock data added

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author' : 'Min Suk',
+        'title' : "Blog Post 1",
+        'content' : "First post content",
+        'date_posted' : 'October 17, 2018'
+    },
+    {
+        'author' : 'Jane Doe',
+        'title' : "Blog Post 2",
+        'content' : "2nd post content",
+        'date_posted' : 'October 27, 2018'
+    },
+]
+
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html')
