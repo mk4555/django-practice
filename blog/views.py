22bf663bd7566caa0a20c571725346ec344ffa3e
Min Suk Kim <minkimcodes@gmail.com>
2018-10-17 23:54:46 -0400

    users app created

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-    {
-        'author' : 'Min Suk',
-        'title' : "Blog Post 1",
-        'content' : "First post content",
-        'date_posted' : 'October 17, 2018'
-    },
-    {
-        'author' : 'Jane Doe',
-        'title' : "Blog Post 2",
-        'content' : "2nd post content",
-        'date_posted' : 'October 27, 2018'
-    },
-]
-
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
